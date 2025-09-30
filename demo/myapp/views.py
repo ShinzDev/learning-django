56ec5d0a107a814f2afa48729c1c5dea7f7da539
Shinaayomi Bamgboye~ <shinaayomibamgboye@gmail.com>
2025-09-30 15:47:46 +0100

    templates added

--- demo/myapp/views.py
+++ demo/myapp/views.py
@@ -1,5 +1,37 @@
 from django.shortcuts import render, HttpResponse
+# from flask import request
 # Create your views here
 
 def home(request):
-    return HttpResponse("All you python devs I am coming 🥵🥵🥵😳😳  ")
+    return render(request, "home.html")
+
+
+
+
+
+
+
+    # return HttpResponse("All you python devs I am coming 🥵🥵🥵😳😳  ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+ 
+ 
+ 
